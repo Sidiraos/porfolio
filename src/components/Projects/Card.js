6f1df81c8553afc91921407683e08209f64eeb61
sidiraos <raossidi@gmail.com>
2023-11-29 00:35:04 +0300

    add : animation on cardProject components

--- src/components/Projects/Card.js
+++ src/components/Projects/Card.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import GitHubIcon from './svgFile/GitHubIcon';
 import LivePreviewIcon from './svgFile/LivePreviewIcon';
+import { useEffect } from 'react';
 
-function Card({ img, title, description, link , codeLink , techStack }){
+function Card({ img, title, description, link, codeLink, techStack, id }) {
+	useEffect(() => {
+		const el = document.getElementById(id);
+		const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("active")
+                    } else {
+                      entry.target.classList.remove("active")
+                    }
+                })
+            }
+        , {rootMargin : "-50px 0px"})
+  
+        observer.observe(el)
+        
+        return () => {
+          observer.disconnect()
+      }
+	}, [id]);
+
+
 	return (
 		<div className="card mb-5">
 			<img src={img} className="card-img-top" alt={title} />
@@ -10,20 +33,35 @@ function Card({ img, title, description, link , codeLink , techStack }){
 				<h5 className="card-title">{title}</h5>
 				<p className="card-text">{description}</p>
 				<p>
-					<span>Tech Stack : </span> <span className='tech-stack-span'>{techStack}</span>
+					<span>Tech Stack : </span>{' '}
+					<span className="tech-stack-span">{techStack}</span>
 				</p>
 			</div>
 			<div className="card-footer">
-				<div className='d-flex justify-content-between'>
-				<a href={link} target="_blank" rel="noopener noreferrer" className='text-dark text-decoration-none'>
-						<LivePreviewIcon />  <span className='text-decoration-underline'>live preview</span>
-				</a>
-				<a href={codeLink} target="_blank" rel="noopener noreferrer" className='text-dark text-decoration-none'>
-						<GitHubIcon /> <span className='text-decoration-underline'>view code</span>
-				</a>
-				
+				<div className="d-flex justify-content-between">
+					<a
+						href={link}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-dark text-decoration-none"
+					>
+						<LivePreviewIcon />{' '}
+						<span className="text-decoration-underline">
+							live preview
+						</span>
+					</a>
+					<a
+						href={codeLink}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-dark text-decoration-none"
+					>
+						<GitHubIcon />{' '}
+						<span className="text-decoration-underline">
+							view code
+						</span>
+					</a>
 				</div>
-		
 			</div>
 		</div>
 	);
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,23 +2,27 @@ import React , {useRef} from 'react';
 import Card from './Card';
 import { projects } from './projectsData';
 import useObserver from '../../customHooks/useObserver';
+
 function Projects() {
   const titleRef = useRef();
   const techStackParagraphRef = useRef();
   useObserver(titleRef);
-  useObserver(techStackParagraphRef)
+  useObserver(techStackParagraphRef);
+ 
+
+
   return (
   <section id="projects" className="py-5 mt-5">
     <div className="container">
       <h2 className="text-center title-style project-title" ref={titleRef}>Featured Projects</h2>
       <p className='text-center tech-stack-paragraph' ref={techStackParagraphRef}>Things I’ve built so far</p>
       <hr className="title-hr" />
-      <div className="row mt-5">
+      <div className="row mt-5 card-container">
         {
             projects.map( item => {
                 return (
-                <div className={`col-md-${6} col-lg-${4} col-12 mb-4`} key={item.id}>
-                        <Card title={item.title} img = {item.img} description = {item.description} link = {item.link} codeLink = {item.codeLink} techStack = {item.techStack} />
+                <div className={`col-md-${6} col-lg-${4} col-12 mb-4 card-parent`} key={item.id} id={item.id} >
+                        <Card title={item.title} img = {item.img} description = {item.description} link = {item.link} codeLink = {item.codeLink} techStack = {item.techStack} id={item.id} />
                 </div>
                 )
             })
